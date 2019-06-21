ae321956178f296652e2916c615fd4e13ae25e5c
Chris Scheib <chris@scheib.io>
2019-06-20 21:17:17 -0400

    light regular keys, not modifiers

--- users/cscheib/rgb_helpers.c
+++ users/cscheib/rgb_helpers.c
@@ -9,7 +9,7 @@
 
 void rgb_matrix_layer_helper (uint8_t red, uint8_t green, uint8_t blue, bool default_layer) {
   for (int i = 0; i < DRIVER_LED_TOTAL; i++) {
-    if (HAS_FLAGS(g_led_config.flags[i], LED_FLAG_MODIFIER)) {
+    if (HAS_FLAGS(g_led_config.flags[i], LED_FLAG_KEYLIGHT)) {
         rgb_matrix_set_color( i, red, green, blue );
     }
   }
