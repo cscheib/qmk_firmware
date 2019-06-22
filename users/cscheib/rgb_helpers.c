c54f2679de7613da8b59878f123508ea7a23af88
Chris Scheib <chris@scheib.io>
2019-06-21 23:59:21 -0400

    it works

--- users/cscheib/rgb_helpers.c
+++ users/cscheib/rgb_helpers.c
@@ -7,8 +7,14 @@
 
 #ifdef RGB_MATRIX_ENABLE
 
+bool is_keycode_kcno(keypos_t keypos) {
+    if(keymap_key_to_keycode(layer_switch_get_layer(keypos), keypos) == KC_NO) {
+        return true;
+    }
+    return false;
+}
+
 void rgb_matrix_layer_render_key(uint8_t led_index, uint8_t switch_layer) {
-    dprintf("rendering led index: %u, switch layer: %u", led_index, switch_layer);
     switch (switch_layer) {
         case _BASE:
         rgb_matrix_set_color(led_index, 0xFF, 0x00, 0x00); break; //TODO: attempt to use the color constants here
@@ -25,18 +31,25 @@ void rgb_matrix_switch_layer_helper (void) {
 
     for(uint8_t row = 0;  row < MATRIX_ROWS; row++) {
         for(uint8_t column = 0;  column < MATRIX_COLS; column++) {
-            dprintf("scanning switch layer: row: %u, column %u\n", row, column);
 
             // actions:
-            // * iterate over whole matrix [x]
-            // * get keypos_t object for key: (keypos_t){ .row = row, .col = column } [x]
+            // * iterate over whole matrix
+            // * get keypos_t object for key: (keypos_t){ .row = row, .col = column }
             // * get switch layer: layer_switch_get_layer(keypos_t)
             // * get led id: g_led_config.matrix_co[row][col]
+            // * evaluate keycode: keymap_key_to_keycode(layer, keypos_t)
+            // * if KC_NO, don't render LED
+
             keypos_t keypos;
             keypos.row = row;
             keypos.col = column;
             uint8_t led_index = g_led_config.matrix_co[row][column];
-            rgb_matrix_layer_render_key(led_index, layer_switch_get_layer(keypos));
+            if(!is_keycode_kcno(keypos)) {
+                rgb_matrix_layer_render_key(led_index, layer_switch_get_layer(keypos));
+            }
+            else {
+                rgb_matrix_set_color(led_index, 0x00, 0x00, 0x00); //set color to white for now
+            }
         }
     }
 }
