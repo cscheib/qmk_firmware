8ec5a679666547d341b9c2da5cb9668b07ea3aae
Chris Scheib <chris@scheib.io>
2019-06-22 01:03:09 -0400

    Merge RGB Key Layer Indication (#1)
    * remove old rgb logic
    * set RGB Matrix such that on/off indicates which keys are active, and color indicates which layer is actually active on a key.

--- users/cscheib/rgb_helpers.c
+++ users/cscheib/rgb_helpers.c
@@ -1,37 +1,67 @@
 #include QMK_KEYBOARD_H
+#include <rgblight_list.h>
 
 #define _BASE   0
 #define _LAYER1 1
 #define _LAYER2 2
 #define _LAYER3 3
 
+#define RGB_OFF 0x00, 0x00, 0x00
+
 #ifdef RGB_MATRIX_ENABLE
 
-void rgb_matrix_layer_helper (uint8_t red, uint8_t green, uint8_t blue, bool default_layer) {
-  for (int i = 0; i < DRIVER_LED_TOTAL; i++) {
-    if (HAS_FLAGS(g_led_config.flags[i], LED_FLAG_KEYLIGHT)) {
-        rgb_matrix_set_color( i, red, green, blue );
+bool is_keycode_kcno(keypos_t keypos) {
+    if(keymap_key_to_keycode(layer_switch_get_layer(keypos), keypos) == KC_NO) {
+        return true;
     }
-  }
+    return false;
+}
+
+void rgb_matrix_layer_render_key(uint8_t led_index, uint8_t switch_layer) {
+    switch (switch_layer) {
+        case _BASE:
+        rgb_matrix_set_color(led_index, RGB_ORANGE); break; //TODO: attempt to use the color constants here
+        case _LAYER1:
+        rgb_matrix_set_color(led_index, RGB_GREEN); break; //TODO: attempt to use the color constants here
+        case _LAYER2:
+        rgb_matrix_set_color(led_index, RGB_BLUE); break; //TODO: attempt to use the color constants here
+        case _LAYER3:
+        rgb_matrix_set_color(led_index, RGB_YELLOW); break; //TODO: attempt to use the color constants here
+    }
+}
+
+void rgb_matrix_switch_layer_helper (void) {
+
+    for(uint8_t row = 0;  row < MATRIX_ROWS; row++) {
+        for(uint8_t column = 0;  column < MATRIX_COLS; column++) {
+
+            // actions:
+            // * iterate over whole matrix
+            // * get keypos_t object for key: (keypos_t){ .row = row, .col = column }
+            // * get switch layer: layer_switch_get_layer(keypos_t)
+            // * get led id: g_led_config.matrix_co[row][col]
+            // * evaluate keycode: keymap_key_to_keycode(layer, keypos_t)
+            // * if KC_NO, don't render LED
+
+            keypos_t keypos;
+            keypos.row = row;
+            keypos.col = column;
+            uint8_t led_index = g_led_config.matrix_co[row][column];
+
+            if(is_keycode_kcno(keypos)) {
+                rgb_matrix_set_color(led_index, RGB_OFF); //set color to black for now
+            }
+            else {
+                rgb_matrix_layer_render_key(led_index, layer_switch_get_layer(keypos));
+            }
+        }
+    }
 }
 
 void rgb_matrix_indicators_user(void) {
-    uint8_t this_led = host_keyboard_leds();
-    if (!g_suspend_state) {
-        switch (biton32(layer_state)) {
-            case _BASE:
-            rgb_matrix_layer_helper(0xFF, 0x00, 0x00, false); break;
-            case _LAYER1:
-            rgb_matrix_layer_helper(0x00, 0xFF, 0x00, false); break;
-            case _LAYER2:
-            rgb_matrix_layer_helper(0x00, 0x00, 0xFF, false); break;
-            case _LAYER3:
-            rgb_matrix_layer_helper(0xFF, 0xFF, 0x00, false); break;
-        }
-    }
-    if ( this_led & (1<<USB_LED_CAPS_LOCK)) {
-        rgb_matrix_set_color(40, 0xFF, 0xFF, 0xFF);
-    }
+    rgb_matrix_switch_layer_helper();
 }
+
+
 
 #endif
